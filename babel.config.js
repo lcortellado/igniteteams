1bbfe3f0c09a71339dadda6cf6365cfed2b63ec5
Luis Carlos Ortellado <luiscarlosortecabral@gmail.com>
2022-10-25 21:58:47 -0300

    Config path mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,23 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "modules-resolver",
+        {
+          root: ["./src"],
+          alias: {
+            "@assets": "./src/assets",
+            "@components": "./src/components",
+            "@routes": "./src/routes",
+            "@screens": "./src/screens",
+            "@storage": "./src/storage",
+            "@theme": "./src/theme",
+            "@utils": "./src/utils",
+          },
+        },
+      ],
+    ],
   };
 };
